86ce585e3d47c2d33d4cab928a395ca548f204fa
YASH JAISWAL <51019768+yashjais@users.noreply.github.com>
2021-04-28 23:57:57 +0530

    made some styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./styles.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  text-aligned: center;
+  margin: 5px;
+`;
+
+const Heading = styled.div`
+  color: red;
+  font-weight: bolder;
+`;
+
+const Paragraph = styled.div`
+  font-size: 44px;
+`;
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Container>
+      <Heading>Hello CodeSandbox</Heading>
+      <Paragraph>Start editing to see some magic happen!</Paragraph>
+    </Container>
   );
 }
